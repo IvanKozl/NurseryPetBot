c07eb473993f422ec7cd77298dce9a9e4895bb74
velik <velikiinkvizitor@mail.ru>
2023-10-17 02:32:09 +0300

    IC

--- src/main/java/com/example/nurserypetbot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/example/nurserypetbot/listener/TelegramBotUpdatesListener.java
@@ -48,59 +48,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                 long chatId = update.message().chat().id();
                 telegramBot.execute(new SendMessage(chatId, "Привет, странник."));
             }
-            //Common help with notification format
-            if(update.message().text().equals("/help")){
-                long chatId = update.message().chat().id();
-                telegramBot.execute(new SendMessage(chatId, "Для создания напоминания введите сообщение в" +
-                        " таком формате: дд.мм.гггг чч:мм текст_напоминания"));
-            }
-            String textMessage = update.message().text();
-            Long chatId = update.message().chat().id();
-            //New notifications method
-            newNotification(chatId,textMessage);
-
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
-    }
-
-    public void newNotification(Long chatId, String textMessage){
-        String dateAndTime;
-        String notificationMessage;
-        LocalDateTime parseDateAndTime;
-        Pattern pattern = Pattern.compile("([0-9\\.\\:\\s]{16})(\\s)(.+)");
-        Matcher matcher = pattern.matcher(textMessage);
-        if (matcher.matches()){
-            dateAndTime = matcher.group(1);
-            notificationMessage = matcher.group(3);
-        }
-        else {
-            telegramBot.execute(new SendMessage(chatId, "Возможно, вы ошиблись при вводе. Нажмите /help чтобы получить подсказку."));
-            return;
-        }
-        try {
-            parseDateAndTime = LocalDateTime.parse(dateAndTime, DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm"));
-        }
-        catch (DateTimeException exception){
-            telegramBot.execute(new SendMessage(chatId,"Неверно введена дата."));
-            return;
-        }
-        notifictionsRepository.save(new Notification(chatId,notificationMessage,parseDateAndTime));
-    }
-
-    //Looking and sending notification for now
-    private void sendNotificationToChat (List<ActualNotification> nList){
-        if(!nList.isEmpty()){
-            for (var n:nList){
-                SendMessage messageToChat = new SendMessage(n.chatId(),n.message());
-                telegramBot.execute(messageToChat);
-            }
-        }
-    }
-    @Scheduled(cron = "0 0/1 * * * *")
-    private void lookingForNotification (){
-        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-        List<ActualNotification> nList;
-        nList = notifictionsRepository.getMessageForNowDate(now);
-        sendNotificationToChat(nList);
     }
 }
