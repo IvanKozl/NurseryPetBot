6736ddb07af6f76df4763b32f5f5015135e1658b
velik <velikiinkvizitor@mail.ru>
2023-10-17 01:19:30 +0300

    IC

---
+++ src/main/java/com/example/nurserypetbot/NurseryPetBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.nurserypetbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseryPetBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseryPetBotApplication.class, args);
+	}
+
+}
